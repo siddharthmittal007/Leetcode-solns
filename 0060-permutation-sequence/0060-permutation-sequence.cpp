0699abcd708e8ebb5c451308ef085fd349ed6ffe
siddharthmittal007 <66960603+siddharthmittal007@users.noreply.github.com>
2024-06-15 17:51:43 +0530

    Time: 0 ms (100%), Space: 7.2 MB (95.99%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    string ans;
-    int factorial(int n) 
-    {
+    // Vector of numbers (1 to n)
+    vector<int> nums;
+    // Vector storing factorials
+    vector <int> factorials;
+
+    void permutationSequence(int n, int k, string& ans) {
+        // Base case
         if (n == 0) 
         {
-            return (1);
-        }
-        return (n * factorial(n - 1));
-    }
-    void permutationSequence(int n,int fact, int k, vector<char>& nums) {
-        if (n == 1) 
-        {
-            ans.push_back(nums[0]);
             return;
         }
 
-        int q = (k - 1) / fact;
-        int r = ((k-1) % fact) + 1;
+        int index = (k - 1) / factorials[n-1];      // Index of digit at current
+                                                    // place in sequence
+        int new_k = ((k-1) % factorials[n-1]) + 1;  // Value of k for subsequence
+                                                    // of (n-1) digits
 
-        ans.push_back(nums[q]);
-        nums.erase(nums.begin()+q);
-        fact=fact/(n-1);
-        permutationSequence(n-1,fact,r,nums);
+        ans.push_back('0'+nums[index]); // adding current digit to string
+        nums.erase(nums.begin()+index); // Deleting digit from stored numbers   
+        
+        permutationSequence(n-1,new_k,ans);   // evalauting subsequence of 
+                                                // (n-1) digits
     }
     string getPermutation(int n, int k) {
-        vector<char> nums;
+        // Initializing vector of numbers (1 to n)
         for(int i=1;i<=n;i++)
         {
-            nums.push_back('1'+(i-1));
+            nums.push_back(i);
         }
-        int fact;
-        fact=factorial(n-1);
-        permutationSequence(n,fact,k,nums);
+
+        // Intializing vector storing factorials
+        int fact=1;
+        factorials.push_back(fact);
+        for(int i=1;i<n;i++)
+        {
+            fact*=i;
+            factorials.push_back(fact);
+        }
+        
+        string ans;
+        permutationSequence(n,k,ans);
         return(ans);
     }
 };
