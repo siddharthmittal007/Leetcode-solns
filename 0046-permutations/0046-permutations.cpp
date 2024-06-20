c36238d4aff54447eae8309e31d87ea50923e4d0
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-06-20 15:05:52 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.94%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> ans; // To store answer permutations
-    
-    void backtrack(vector<int>& nums, int index, vector<int>& v) 
+
+    void backtrack(vector<int>& nums, int index) 
     {
         // Base Case(saving solution)
         if (index == nums.size()) 
         {
-            ans.push_back(v);
+            ans.push_back(nums);
             return;
         }
 
         for (int i = index; i < nums.size(); i++) 
         {
-            v.push_back(nums[i]);
             swap(nums[i],nums[index]);
-
-            backtrack(nums,index+1,v);
-
-            v.pop_back();
+            backtrack(nums,index+1);
             swap(nums[i],nums[index]);
             
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> v;              // To store permutations
-        backtrack(nums,0,v);
+        backtrack(nums,0);
         return (ans);
     }
 };
