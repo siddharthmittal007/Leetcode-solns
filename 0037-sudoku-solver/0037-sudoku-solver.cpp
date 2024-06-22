26ce9a80db654958100ae984252373a3b867e4fa
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-06-22 18:29:01 +0530

    Time: 3 ms (97.16%), Space: 8.9 MB (14.06%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:    
-    // Vectors to hold occupancy of digits by rows, columns and squares
-    vector<vector<bool>> row;
-    vector<vector<bool>> column;
-    vector<vector<bool>> square;
-
-    bool backtrack(vector<vector<char>>& board,int index)
+    bool backtrack(vector<vector<char>>& board,int index,vector<vector<bool>>& row,vector<vector<bool>>& column,vector<vector<bool>>& square)
     {
         if(index==9*9) 
         {
@@ -19,10 +14,10 @@ public:
 
         if(board[r][c]!='.') // Index already filled
         {
-            return(backtrack(board,index+1)); // Proceed to next index
+            return(backtrack(board,index+1,row,column,square)); // Proceed to next index
         }
         
-        // Checking eah digit from 1-9 for valid placement
+        // Checking each digit from 1-9 for valid placement
         for(int i=0;i<9;i++)
         {
             if(row[r][i]==true || column[c][i]==true || square[s][i]==true)
@@ -34,40 +29,40 @@ public:
             board[r][c]='1'+i;
             row[r][i]=column[c][i]=square[s][i]=true;
 
-            if(backtrack(board,index+1)==false)
+            if(backtrack(board,index+1,row,column,square)==true)
             {
-                board[r][c]='.';
-                row[r][i]=column[c][i]=square[s][i]=false;
-                continue;
+                return(true);
             }
-            return(true);
+            
+            board[r][c]='.';    
+            row[r][i]=column[c][i]=square[s][i]=false;
+            
         }
 
         return(false);
     }
     
     void solveSudoku(vector<vector<char>>& board) {
-        // Initializing occupancy vectors
-        row=vector<vector<bool>> (9,vector<bool>(9,false));
-        column=vector<vector<bool>> (9,vector<bool>(9,false));
-        square=vector<vector<bool>> (9,vector<bool>(9,false));
+        // Vectors to hold occupancy of digits by rows, columns and squares
+        vector<vector<bool>> row(9,vector<bool>(9,false));
+        vector<vector<bool>> column(9,vector<bool>(9,false));
+        vector<vector<bool>> square(9,vector<bool>(9,false));
         
         // Marking occurence of digits in given filled cells
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
-                if(board[i][j]=='.')
+                if(board[i][j]!='.')
                 {
-                    continue;
+                    row[i][board[i][j]-'1']=true;
+                    column[j][board[i][j]-'1']=true;
+                    square[i-(i%3)+j/3][board[i][j]-'1']=true;
                 }
-
-                row[i][board[i][j]-'1']=true;
-                column[j][board[i][j]-'1']=true;
-                square[i-(i%3)+j/3][board[i][j]-'1']=true;
+                
             }
         }
 
-        backtrack(board,0); // Beginning solving from 0th index
+        backtrack(board,0,row,column,square); // Beginning solving from 0th index
     }
 };
