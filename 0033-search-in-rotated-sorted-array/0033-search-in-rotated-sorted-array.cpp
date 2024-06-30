89193d7744d34ffa7cf511cb0d18f413e7d90d77
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-06-30 13:43:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // Binary search
+        int low=0,high=nums.size()-1;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target)
+            {
+                return(mid);    // 'target' found
+            }
+            
+            else if(low<=mid)
+            {
+                if(target>=nums[low] && target<nums[mid])
+                {
+                    high=mid-1;
+                }
+                else    // target>nums[mid] || target<nums[low]
+                {
+                    low=mid+1;
+                } 
+            }
+            else    // mid<low
+            {
+                if(target<nums[low] && target>nums[mid])
+                {
+                    low=mid+1;
+                }
+                else    // target<nums[mid] || target>=nums[low]
+                {
+                    high=mid-1;
+                }
+            }
+        }
+
+        return(-1);     // 'target' not found
+
+        
+    }
+};
