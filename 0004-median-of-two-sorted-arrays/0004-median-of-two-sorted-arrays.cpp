bb85c66c2f5bac1702682274044731879154ecf0
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-01 14:52:49 +0530

    Time: 38 ms (8.93%), Space: 94.5 MB (57.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size(),n=nums2.size(); 
+        int m=nums1.size(),n=nums2.size();  // Sizes
         
-        int low1=-1,low2=-1;            
+        int low1=-1,low2=-1;                // Lower index bounds on vectors
 
         int k=(m+n+1)/2;                    // Position of (first)median element
 
+        // Successively halving 'k' at each step (by covering removed half)
+        // and increasing lower bounds
         while(k>0)
         {
             int half=k-k/2;
-            
             if(low1==m-1)           // No more elements in 'nums1'
             {
                 low2+=half;
                 k-=half;
             }
-
-            else if(low2==n-1)      // No more elemnts in 'nums2'            
+            else if(low2==n-1)      // No more elements in 'nums2'            
             {
                 low1+=half;
                 k-=half;
             }
-
             else
             {
                 int ind1=min(m-1,low1+half);
                 int ind2=min(n-1,low2+half);
                 
-                if(nums1[ind1]<=nums2[ind2])
+                if(nums1[ind1]<=nums2[ind2])    
                 {
                     k-=ind1-low1;
                     low1=ind1;
