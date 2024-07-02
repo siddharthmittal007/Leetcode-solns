17071e0f1881285e5d52e08534d641cb7b620218
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-02 22:22:17 +0530

    Time: 23 ms (68.83%), Space: 94.5 MB (57.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,49 +1,5 @@
 class Solution {
 public:
-    int solve(vector<int>& nums1,vector<int>& nums2,int k)
-    {   
-        int m=nums1.size(),n=nums2.size();  // Sizes
-        
-        int low=0,high=m-1;
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            int FL=mid;
-            int FR=FL+1;
-            int SL=k-mid-2;
-            int SR=SL+1;
-
-            if(SL>=n)
-            {
-                low=mid+1;
-            }
-            else if(SL<0)
-            {
-                if(n!=0 && nums2[0]<nums1[FL])
-                high=mid-1;
-                else
-                {
-                    return(nums1[FL]);
-                }
-            }
-            else if ((SR==n || nums1[FL]<=nums2[SR]) &&
-                    (FR==m || nums2[SL]<=nums1[FR]))
-            {
-                return(max(nums1[FL],nums2[SL]));   // Element found
-            }
-            else if(SR!=n && nums1[FL]>nums2[SR])
-            {
-                high=mid-1;
-            }
-            else    //(FR!=m && nums2[SL]>nums1[FR])
-            {
-                low=mid+1;
-            }
-            
-        }  
-        return(nums2[k-1]); 
-    }
-
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         // Ensuring 'nums1' of lesser(or equal) size to 'nums2'
         if(nums1.size()>nums2.size())
@@ -55,13 +11,41 @@ public:
     
         int k=(m+n+1)/2;                    // Position of(first)median element
 
-        if((m+n)%2==1)                      // Odd number of elements
+        // Binary search
+        int low=0,high=m;
+        while(low<=high)
         {
-            return(1.0*solve(nums1,nums2,k));
-        }
-        else                                // Even number of elements                                                
-        {
-            return((1.0*(solve(nums1,nums2,k)+solve(nums1,nums2,k+1)))/2);
-        }
+            int partitionA=low+(high-low)/2;
+            int partitionB=k-partitionA;
+
+            int LeftA=(partitionA==0?INT_MIN:nums1[partitionA-1]);
+            int RightA=(partitionA==m?INT_MAX:nums1[partitionA]);
+            int LeftB=(partitionB==0?INT_MIN:nums2[partitionB-1]);
+            int RightB=(partitionB==n?INT_MAX:nums2[partitionB]);
+
+            if(LeftA<=RightB && LeftB<=RightA)
+            {
+                // Median element(s) found
+                if((m+n)%2==1)      // Single median
+                {
+                    return(max(LeftA,LeftB));
+                }
+                else                // Two medians
+                {
+                    return  ((1.0*(max(LeftA,LeftB)+
+                            min(RightA,RightB)))/2);
+                }
+            }
+            else if(LeftA>RightB)
+            {
+                high=partitionA-1;
+            }
+            else    // (LeftB>RightA)
+            {
+                low=partitionA+1;
+            }        
+        } 
+
+        return(0); 
     }
 };
