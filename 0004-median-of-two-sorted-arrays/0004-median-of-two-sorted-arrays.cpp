ec651bb3b5e2bcd74da07a1c06e94bf74ac1fab3
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-04 15:12:36 +0530

    Time: 19 ms (85.09%), Space: 94.3 MB (97.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,48 @@
 class Solution {
 public:
-    int solve(vector<int>& nums1,int low1,vector<int>& nums2,int low2,int k)
+    int solve(vector<int>& nums1,int low1,int high1,vector<int>& nums2,int low2,int high2,int k)
     {   
-        int m=nums1.size(),n=nums2.size();  // Sizes
-        
-        if(low1==m || low2==n)
+        if(low1>high1)      // 'nums1' empty
         {
-            return(low1==m?nums2[low2+k-1]:nums1[low1+k-1]);
+            return(nums2[k-low1-1]);
         }
-        else
+        else if(low2>high2) // 'nums2' empty
         {
-            if(k==1)
-            {
-                return(nums1[low1]<=nums2[low2]?nums1[low1]:nums2[low2]);
-            }
-            int half=k/2;
-            int ind1=min(m-1,low1+half-1);
-            int ind2=min(n-1,low2+half-1);
+            return(nums1[k-low2-1]);
+        }
+        else                // 'nums1' and 'nums2' non-empty
+        {
+            int mid1=low1+(high1-low1)/2;
+            int mid2=low2+(high2-low2)/2;
+            int count=(mid1+1)+(mid2+1);
 
-            if(nums1[ind1]<=nums2[ind2])
+            if(count<=k)        // Eliminate first half
             {
-                k-=ind1-low1+1;
-                return(solve(nums1,ind1+1,nums2,low2,k));
+                if(nums1[mid1]<=nums2[mid2])        
+                {
+                    // Eliminate first half of 'nums1'
+                    return(solve(nums1,mid1+1,high1,nums2,low2,high2,k));
+                }
+                else
+                {
+                    // Eliminate first half of 'nums2'
+                    return(solve(nums1,low1,high1,nums2,mid2+1,high2,k));
+                }
             }
-            else
+            else                // Eliminate second half
             {
-                k-=ind2-low2+1;
-                return(solve(nums1,low1,nums2,ind2+1,k));
+                if(nums1[mid1]<=nums2[mid2])
+                {
+                    // Eliminate second half of 'nums2'
+                    return(solve(nums1,low1,high1,nums2,low2,mid2-1,k));
+                }
+                else
+                {
+                    // Eliminate second half of 'nums1'
+                    return(solve(nums1,low1,mid1-1,nums2,low2,high2,k));
+                }
             }
-        }
+        }       
     }
 
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -38,11 +52,11 @@ public:
 
         if((m+n)%2==1)                      // Odd number of elements
         {
-            return(1.0*solve(nums1,0,nums2,0,k));
+            return(1.0*solve(nums1,0,m-1,nums2,0,n-1,k));
         }
         else                                // Even number of elements                                                
         {
-            return((1.0*(solve(nums1,0,nums2,0,k)+solve(nums1,0,nums2,0,k+1)))/2);
+            return((1.0*(solve(nums1,0,m-1,nums2,0,n-1,k)+solve(nums1,0,m-1,nums2,0,n-1,k+1)))/2);
         }
     }
 };
