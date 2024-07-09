198049d3341d9e3e9e32aea3c1d0f3528491a9a0
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-09 17:20:41 +0530

    Update 0295-find-median-from-data-stream-Simple.cpp

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream-Simple.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream-Simple.cpp
@@ -1,31 +1,31 @@
 class MedianFinder {
 public:
-    priority_queue<int> max_pq;
-    priority_queue<int,vector<int>,greater<int>> min_pq; 
+    priority_queue<int> max_pq;                                 // Max Heap
+    priority_queue<int,vector<int>,greater<int>> min_pq;        // Min Heap
 
     MedianFinder() {
     }
     
-    void addNum(int num) {
-        if(max_pq.size()==0)
+    void addNum(int num) 
+    {
+        if(max_pq.size()==0 && min_pq.size()==0)    // No elements
         {
             max_pq.push(num);
         }
-        else if(max_pq.size()==min_pq.size())
+        else if(max_pq.size()==min_pq.size())       // Heaps of equal size
         {
-            if(min_pq.top()>=num )
+            if(min_pq.top()>=num)
             {
                 max_pq.push(num);
             }
             else
             {
-                int temp=min_pq.top();
+                max_pq.push(min_pq.top());
                 min_pq.pop();
                 min_pq.push(num);
-                max_pq.push(temp);
             }
         }
-        else
+        else                                        // Max Heap has greater size
         {
             if(num>=max_pq.top())
             {
@@ -33,21 +33,20 @@ public:
             }
             else
             {
-                int temp=max_pq.top();
+                min_pq.push(max_pq.top());
                 max_pq.pop();
-                max_pq.push(num);
-                min_pq.push(temp);
+                max_pq.push(num);   
             }
-        }
-        
+        }  
     }
     
-    double findMedian() {
-        if(min_pq.size()==max_pq.size())
+    double findMedian() 
+    {
+        if(min_pq.size()==max_pq.size())                // Even elements
         {
             return((min_pq.top()+max_pq.top())/2.0);
         }
-        else
+        else                                            // Odd elements
         {
             return(1.0*max_pq.top());
         }
