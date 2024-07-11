b423fdae25c6b36d6e39e31be348654d35da8306
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-11 13:48:03 +0530

    Time: 10 ms (76.08%), Space: 16.9 MB (80.73%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elementsump+pq(O(K(logN))).cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    // Custom Compare
+    struct comp{
+        bool operator()(pair<int,int> A,pair<int,int> B)
+        {
+            return(A.second<B.second);
+        }
+    };
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Inserting elements to unordered map 
+        // and maintaining frequency count of element
+        unordered_map<int,int> ump;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(ump.find(nums[i])==ump.end())
+            {
+                ump.insert({nums[i],1});
+            }
+            else
+            {
+                ump.at(nums[i])++;
+            }
+        }
+
+        // Max heap on key 'frequency' of elements
+        typedef pair<int,int> pii;
+        priority_queue<pii,vector<pii>,comp> pq(ump.begin(),ump.end());
+
+        vector<int> ans;    // vector to store 'k' most frequent elements
+
+        // Obtaining 'k' most frequent elements
+        for(int i=1;i<=k;i++)
+        {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+        
+        return(ans);        // Return 'k' most frequent elements
+    }
+};
+
+// T.C=O(KlogN) ;   S.C=O(N)
