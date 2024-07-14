40b17405fc1ccdfe1326f3e3c89fd2bf8531b54c
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-14 17:50:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (6.37%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -19,64 +19,79 @@ public:
     
     int pop() {
         if(q1.size()==0)
-        {
+        {   
+            // Transferring all elements (except 'top' element) to 'q1'
             while(q2.size()>1)
             {
                 q1.push(q2.front());
                 q2.pop();
             }
+
             int temp=q2.front();
             q2.pop();
-            return(temp);
+            return(temp);   // Returning 'top' element
         }
         else
         {
+            // Transferring all elements (except 'top' element) to 'q2'
             while(q1.size()>1)
             {
                 q2.push(q1.front());
                 q1.pop();
             }
+            
             int temp=q1.front();
             q1.pop();
-            return(temp);
+            return(temp);   // Returning 'top' element
         }
     }
     
     int top() {
         if(q1.size()==0)
         {
+            // Transferring all elements (except 'top' element) to 'q1'
             while(q2.size()>1)
             {
                 q1.push(q2.front());
                 q2.pop();
             }
+            
             int temp=q2.front();
             q2.pop();
-            q1.push(temp);
-            return(temp);
+            q1.push(temp);      // Transferring 'top' element
+            return(temp);       // Return 'top' element
         }
         else
         {
+            // Transferring all elements (except 'top' element) to 'q2'
             while(q1.size()>1)
             {
                 q2.push(q1.front());
                 q1.pop();
             }
+            
             int temp=q1.front();
             q1.pop();
-            q2.push(temp);
-            return(temp);
+            
+            q2.push(temp);      // Transferring 'top' element
+            return(temp);       // Return 'top' element
         }
     }
     
     bool empty() {
         if(q1.size()==0 && q2.size()==0)
         {
-            return (true);
+            return (true);  // Stack empty
         }
-        return(false);        
+        return(false);      // Stack non-empty     
     }
 };
+
+// PUSH
+// T.C=O(1) ;   S.C=O(1)
+
+// POP
+// T.C=O(N) ;   S.C=O(1)
 
 /**
  * Your MyStack object will be instantiated and called as such:
