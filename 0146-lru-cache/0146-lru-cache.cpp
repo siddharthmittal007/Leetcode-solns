ea977cd262fe82b086d75e699c427e2f78c6bd69
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-17 15:06:37 +0530

    Time: 330 ms (78.32%), Space: 170.1 MB (68.74%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -20,59 +20,29 @@ public:
     
     Node *head, *tail;                  // 'head' and 'tail' pointers to DLL queue
     
-    void move_to_tail(Node *node)
-    {
-        if(node==tail)
-        {
-            return;     // node already at tail
-        }
-        
-        // Removing from current position
-        if(node==head)
-        {
-            node->next->prev=NULL;
-            head=node->next;
-        }
-        else
-        {
-            node->prev->next=node->next;
-            node->next->prev=node->prev;
-        }
-                
-        // Moving to tail
-        node->next=NULL;
-        node->prev=tail;
-        tail->next=node;
-        tail=node;
-    }
-
-    void delete_head(Node *node)
-    {
-        // Remove from DLL queue
-        if(head==tail)
-        {
-            head=tail=NULL;
-        }  
-        else
-        {
-            node->next->prev=NULL;
-            head=node->next;
-        }  
-
-        cache.erase(node->Key);     // Delete from cache
-        delete(node);               // Free memory
-    }
-    
     LRUCache(int capacity) {
         max_cap=capacity;
-        head=tail=NULL;
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
+        head->next=tail;
+        tail->prev=head;
     }
     
     int get(int key) {
         if(cache.find(key)!=cache.end())    // Key exists in cache
         {
             Node *node=cache.at(key);   
-            move_to_tail(node);
+            
+            // Removing from current position
+            node->prev->next=node->next;
+            node->next->prev=node->prev;
+            
+            // Move to tail
+            node->next=tail;
+            node->prev=tail->prev;
+            tail->prev->next=node;
+            tail->prev=node;
+
             return(node->Value);          
         }
         return(-1);     // Key does not exist in cache
@@ -84,28 +54,39 @@ public:
             // Update key value
             Node *node=cache.at(key);
             node->Value=value;
-            move_to_tail(node);
+
+            // Removing from current position
+            node->prev->next=node->next;
+            node->next->prev=node->prev;
+            
+            // Move to tail
+            node->next=tail;
+            node->prev=tail->prev;
+            tail->prev->next=node;
+            tail->prev=node;
+
             return;
         }
         
         if(cache.size()==max_cap)   // Evict LRU element
         {   
-            delete_head(head);
+            Node *node=head->next;
+            head->next=node->next;
+            node->next->prev=head;
+
+            cache.erase(node->Key);     // Delete from cache
+            delete(node);               // Free memory
         }
 
         // Add key to DLL queue 'tail'
         Node *node=new Node(key,value);
-        if(head==NULL && tail==NULL)
-        {
-            head=tail=node;
-        }
-        else
-        {
-            node->prev=tail;
-            tail->next=node;
-            tail=node;
-        }
         
+        // Move to tail
+        node->next=tail;
+        node->prev=tail->prev;
+        tail->prev->next=node;
+        tail->prev=node;
+
         cache.insert({key,node});           // Add (key,node_ptr) pair to cache
     }
 };
