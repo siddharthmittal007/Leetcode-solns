cd959a303fcf99a8c5b79463798eeb589064c74d
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-17 14:53:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,55 +1,111 @@
+// DLL node definition
+class Node{
+    public:
+        int Key;
+        int Value;
+        Node *prev;
+        Node *next;
+        Node(int _Key,int _Value)
+        {
+            Key=_Key;
+            Value=_Value;
+            prev=next=NULL;
+        }
+};
+
 class LRUCache {
 public:
-    unordered_map<int,int> cache;   // To store (key,value) pairs
-    int max_cap;                    // Max capacity of cache
+    unordered_map<int,Node*> cache;     // To store (key,value) pairs
+    int max_cap;                        // Max capacity of cache
     
-    queue<int> q;                   // FIFO queue of 'key' use(put/get)
-    
-    unordered_map<int,int> cnt;     // Store number of entries in FIFO queue
-                                    // corresponding to key 'key'
-    
+    Node *head, *tail;                  // 'head' and 'tail' pointers to DLL queue
     
     LRUCache(int capacity) {
         max_cap=capacity;
+        head=tail=NULL;
     }
     
     int get(int key) {
         if(cache.find(key)!=cache.end())    // Key exists in cache
         {
-            q.push(key);                    // Add key to FIFO queue
-            cnt[key]++;                     // Increment number of entries in FIFO queue
-            return(cache.at(key));          // Return key value
+            Node *node=cache.at(key);   // 'key' node in DLL queue
+            
+            if(node!=tail)              // Move node to tail
+            {
+                // Removing node from current position 
+                if(node->prev!=NULL)
+                {
+                    node->prev->next=node->next;
+                }
+                if(node->next!=NULL)
+                {
+                    node->next->prev=node->prev;
+                }
+                // Setting head
+                if(node==head)
+                {
+                    head=node->next;
+                }
+                
+                // Moving to tail
+                node->next=NULL;
+                node->prev=tail;
+                tail->next=node;
+                tail=node;
+            }
+            
+            return(node->Value);          // Return value corresp. to key
         }
+        
         return(-1);     // Key does not exist in cache
     }
     
     void put(int key, int value) {
-        if(cache.find(key)==cache.end() && cache.size()==max_cap)
+        if(cache.find(key)!=cache.end())    // key exists in cache   
         {
-            // Finding least recently used element
-            while(!q.empty())
+            // Update key value
+            Node *node=cache.at(key);
+            node->Value=value;
+            return;
+        }
+        
+        if(cache.size()==max_cap)   // Evict LRU element
+        {
+            Node *node=head;        // LRU element at DLL queue 'head'
+            
+            // Delete from DLL queue
+            if(node==tail)
             {
-                int temp=q.front();
-                q.pop();
-                cnt[temp]--;
-                if(cnt[temp]==0)
-                {
-                    // Evict least recently used element
-                    cnt.erase(temp);
-                    cache.erase(temp);
-                    break;
-                }
+                head=tail=NULL;
             }
+            else
+            {
+                node->next->prev=NULL;
+                head=node->next;
+            }
+            
+            cache.erase(node->Key); // Delete from cache
+            delete(node);           // Free memory
         }
 
-        cache[key]=value;           // Add/Update (key,value) pair to cache
-        q.push(key);                // Add key to FIFO queue
-        cnt[key]++;                 // Increment number of entries of key 
-                                    // in FIFO queue
+        // Add key to DLL queue 'tail'
+        Node *node=new Node(key,value);
+        if(head==NULL && tail==NULL)
+        {
+            head=tail=node;
+        }
+        else
+        {
+            node->prev=tail;
+            tail->next=node;
+            tail=node;
+        }
+        
+        cache.insert({key,node});           // Add (key,node_ptr) pair to cache
     }
 };
 
-// T.C=O(1) (PUT AND GET OPERATIONS)
+// T.C=O(1) (PUT)   ;   T.C=O(1) (GET)
 // S.C=O(N)
 
 /**
