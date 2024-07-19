bb5d00bc5cd6919fc4dad2c6acf15ba0b7c3401f
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-19 15:53:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,49 +1,28 @@
-// DLL node definition
-class Node{
-    public:
-        int Key;
-        int Value;
-        Node *prev;
-        Node *next;
-        Node(int _Key,int _Value)
-        {
-            Key=_Key;
-            Value=_Value;
-            prev=next=NULL;
-        }
-};
-
 class LRUCache {
 public:
-    unordered_map<int,Node*> cache;     // To store (key,value) pairs
-    int max_cap;                        // Max capacity of cache
+    unordered_map<int,list<pair<int,int>>::iterator> cache; // To store (key,value)
+                                                            // pairs
+    int max_cap;                                            // Max capacity of cache
     
-    Node *head, *tail;                  // 'head' and 'tail' pointers to DLL queue
+    list<pair<int,int>>  q;                                 // DLL queue
     
     LRUCache(int capacity) {
         max_cap=capacity;
-        head=new Node(-1,-1);
-        tail=new Node(-1,-1);
-        head->next=tail;
-        tail->prev=head;
     }
     
     int get(int key) {
         if(cache.find(key)!=cache.end())    // Key exists in cache
         {
-            Node *node=cache.at(key);   
+            auto it=cache.at(key);
             
             // Removing from current position
-            node->prev->next=node->next;
-            node->next->prev=node->prev;
-            
-            // Move to tail
-            node->next=tail;
-            node->prev=tail->prev;
-            tail->prev->next=node;
-            tail->prev=node;
+            pair<int,int> elem=make_pair(it->first,it->second);
+            q.erase(it);
 
-            return(node->Value);          
+            // Move to back
+            q.push_back(elem);   
+            
+            return(elem.second);          
         }
         return(-1);     // Key does not exist in cache
     }
@@ -52,42 +31,32 @@ public:
         if(cache.find(key)!=cache.end())    // key exists in cache   
         {
             // Update key value
-            Node *node=cache.at(key);
-            node->Value=value;
+            auto it=cache.at(key);
+            it->second=value;
 
             // Removing from current position
-            node->prev->next=node->next;
-            node->next->prev=node->prev;
-            
-            // Move to tail
-            node->next=tail;
-            node->prev=tail->prev;
-            tail->prev->next=node;
-            tail->prev=node;
+            pair<int,int> elem=make_pair(it->first,it->second);
+            q.erase(it);
+
+            // Move to back
+            q.push_back(elem);
 
             return;
         }
         
-        if(cache.size()==max_cap)   // Evict LRU element
+        if(cache.size()==max_cap)   // Evict LRU element(at queue front)
         {   
-            Node *node=head->next;
-            head->next=node->next;
-            node->next->prev=head;
+            // Remove from DLL queue
+            pair<int,int> elem=make_pair(q.front().first,q.front().second);
+            q.pop_front();
 
-            cache.erase(node->Key);     // Delete from cache
-            delete(node);               // Free memory
+            cache.erase(elem.first);     // Delete from cache
         }
 
-        // Add key to DLL queue 'tail'
-        Node *node=new Node(key,value);
-        
-        // Move to tail
-        node->next=tail;
-        node->prev=tail->prev;
-        tail->prev->next=node;
-        tail->prev=node;
+        // Add key to DLL queue back
+        auto it=q.insert(q.end(),make_pair(key,value));
 
-        cache.insert({key,node});           // Add (key,node_ptr) pair to cache
+        cache.insert(make_pair(key,it));           // Add (key,iterator) pair to cache
     }
 };
 
