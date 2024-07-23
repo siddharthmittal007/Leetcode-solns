65c6260331f73ee8fdd4766129263efd5c31520f
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-23 15:37:47 +0530

    Time: 107 ms (75.26%), Space: 79.4 MB (79.89%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -7,14 +7,17 @@ public:
         stack<elem> s;      // Monotonic increasing stack of
                             // (ind,(height,prev_ind)) elements
 
+        // Traversing vector and adding elements to monotonic stacks 
         for(int i=0;i<heights.size();i++)
         {
             while(!s.empty() && s.top().second.first>=heights[i])
             {
+                // Update max area on elem pop
                 int ht=s.top().second.first,prev_ind=s.top().second.second;        
                 max_area=max(max_area,(i-prev_ind)*ht);
                 s.pop();
             }
+            // Push current element
             if(s.empty())
             {
                 s.push(make_pair(i,make_pair(heights[i],0)));
@@ -25,20 +28,15 @@ public:
             }  
         }
 
+        // Empty stack iteratively and update max area value
         int n=heights.size();
         while(!s.empty())
         {
-            int ht=s.top().second.first,prev_ind=s.top().second.second;
-                    
+            int ht=s.top().second.first,prev_ind=s.top().second.second;  
             max_area=max(max_area,(int)((n-prev_ind)*ht));
             s.pop();
         }
-        return(max_area);
+        
+        return(max_area);   // return max area
     }
 };
-
-auto init = []() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  return '$';
-}();
