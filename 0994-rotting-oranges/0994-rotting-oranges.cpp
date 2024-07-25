350d082dcb66bcf1b1ba90cf9156acb14234d96e
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-07-25 14:11:14 +0530

    Time: 0 ms (100%), Space: 16.3 MB (33.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,71 +1,59 @@
 class Solution {
-public:
-    int bfs(vector<vector<int>>& grid,vector<pair<int,int>> &directions,int i,int j)
-    {
-        int minutes=0;              // Minutes taken
-        
-        queue<pair<int,int>> q;     // Queue to implement bfs
-        
-        // Pushing starting point to queue
-        grid[i][j]=0;
-        q.push({i,j});
-        
-        q.push({-1,-1});    // For minute++
-
-        // bfs
-        while(!q.empty())
-        {
-            int r=q.front().first,c=q.front().second;
-            q.pop();
-            
-            // minute++
-            if(r==-1 && c==-1)
-            {
-                if(q.empty())
-                {
-                    break;
-                }   
-                minutes++;
-                q.push({-1,-1});
-                continue;
-            }
-            
-            // Visiting 4-Directional neighbours of current cell
-            for(int cnt=0;cnt<4;cnt++)
-            {
-                int _r=r+directions[cnt].first,_c=c+directions[cnt].second;
-                if  ((0<=_r && _r<grid.size()) && (0<=_c && _c<grid[0].size())
-                    && grid[_r][_c]==1)
-                {
-                    grid[_r][_c]=0;
-                    q.push({_r,_c});
-                }
-            }
-        }
-
-        return(minutes);
-    }
-    
-    
+public:    
     int orangesRotting(vector<vector<int>>& grid) {
-        int minutes=0;      // Minimum number of minutes
+        int minutes=0;              // Minimum number of minutes
 
         vector<pair<int,int>> directions=
                         {{0,1},{0,-1},{1,0},{-1,0}};    // 'N' 'S' 'E' 'W'
         
-        // Calling bfs
+        queue<pair<int,int>> q;     // Queue to implement bfs
+        
+        // Pushing rotten oranges to queue
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[0].size();j++)
             {
                 if(grid[i][j]==2)
                 {
-                    minutes=max(minutes,bfs(grid,directions,i,j));
+                    grid[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
 
-        // Checking if fresh orange left
+        q.push({-1,-1});            // To keep track of minutes
+
+        // Breadth First Propogation
+        while(!q.empty())
+        {
+            int i=q.front().first,j=q.front().second;
+            q.pop();
+            
+            if(i==-1 && j==-1)
+            {
+                if(!q.empty())
+                {
+                    // Increment minutes
+                    minutes++;
+                    q.push({-1,-1});
+                }   
+                continue;
+            }
+            
+            // Visiting 4-Directional neighbours of current cell
+            for(int cnt=0;cnt<4;cnt++)
+            {
+                int r=i+directions[cnt].first,c=j+directions[cnt].second;
+                if  ((0<=r && r<grid.size()) && (0<=c && c<grid[0].size())
+                    && grid[r][c]==1)
+                {
+                    grid[r][c]=0;
+                    q.push({r,c});
+                }
+            }
+        }
+
+        // Checking if any fresh orange left
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[0].size();j++)
@@ -77,6 +65,11 @@ public:
             }
         }
 
-        return(minutes);
+        return(minutes);    // Minimum number of minutes for all oranges 
+                            // to get rotten
     }
 };
+
+// T.C=O(M*N)   ;`S.C=O(M*N)
+
+// put all rootten oranges in calling funtion itself and dont create extra fucntion
