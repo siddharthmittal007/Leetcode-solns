31b02c3a2202201dd46f3c9b488738cee9b8b75f
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-08-12 14:41:31 +0530

    Time: 4 ms (57.65%), Space: 8.6 MB (79.98%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -25,56 +25,75 @@ public:
             swap(s[start++],s[end--]);
         }
     }
+
+    //Utility function
+    int wordEnd(string& s,int pos)
+    {
+        while(pos<s.size()-1 && s[pos+1]!=' ')
+        {
+            pos++;
+        }
+        return(pos);
+    }
+    
+    //Utility function
+    int wordBegin(string& s,int pos)
+    {
+        while(pos>0 && s[pos-1]!=' ')
+        {
+            pos--;
+        }
+        return(pos);
+    }
     
     string reverseWords(string s) {
+        // Removing extra space
         removeExtraSpace(s);
+
+        // If only one word
+        if(wordEnd(s,0)==s.size()-1)
+        {
+            return(s);
+        }
 
         int fwd1=0,fwd2=0;
         int bwd1=s.size()-1,bwd2=s.size()-1;
 
-        while(fwd2<s.size()-1 && s[fwd2+1]!=' ')
-            fwd2++;
+        // Reversing first and last words
+        fwd2=wordEnd(s,fwd1);
         reverse(s,fwd1,fwd2);    
         
-        while(bwd2>0 && s[bwd2-1]!=' ')
-            bwd2--;
+        bwd2=wordBegin(s,bwd1);
         reverse(s,bwd2,bwd1);
 
         
-        while(fwd2<bwd2)
+        // Reversing word order
+        while(true)
         {
-            while(fwd1<=fwd2 && bwd2<=bwd1)
+            while(fwd1<=fwd2+1 && bwd2-1<=bwd1)
             {
                 swap(s[fwd1++],s[bwd1--]);
             }
 
-            if(fwd2<fwd1)
-            {
-                swap(s[fwd1++],s[bwd1--]);    // Adding space to reverse forming string
-                
-                if(fwd1==bwd2)      // Copy rest of word to forward forming string  
+            if(fwd2+1<fwd1)    
+            {   
+                if(fwd1==bwd2)       
                 {
                     reverse(s,bwd2,bwd1);
                     break;
                 }
-                fwd2=fwd1;
-                while(fwd2!=s.size()-1 && s[fwd2+1]!=' ')
-                    fwd2++;
+                fwd2=wordEnd(s,fwd1);
                 reverse(s,fwd1,fwd2);
             }
 
-            if(bwd1<bwd2)
-            {
-                swap(s[fwd1++],s[bwd1--]);     // Adding space to reverse forming string
-                
+            if(bwd1<bwd2-1)
+            {   
                 if(bwd1==fwd2)      // Copy rest of word to forward forming string  
                 {
                     reverse(s,fwd1,fwd2);
                     break;
                 }
-                bwd2=bwd1;
-                while(bwd2!=0 && s[bwd2-1]!=' ')
-                    bwd2--;
+                bwd2=wordBegin(s,bwd1);
                 reverse(s,bwd2,bwd1);
             }
         }
