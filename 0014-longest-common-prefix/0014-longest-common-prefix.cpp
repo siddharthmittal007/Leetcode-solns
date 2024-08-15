fd8134c20e5143c241642bb37317169514b4e091
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-08-15 14:29:55 +0530

    Time: 0 ms (100%), Space: 11.2 MB (59.43%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // Calculating max possible length of prefix
-        size_t max_len=INT_MAX;
-        for(int i=0;i<strs.size();i++)
-        {
-            max_len=min(max_len,strs[i].size());
-        }
-
-        // LCP string
         string lcp;
         
         // Finding LCP
-        for(int i=0;i<max_len;i++)
+        for(int i=0;i<strs[0].size();i++)
         {
             char c=strs[0][i];
-            int j=1;
-            for(;j<strs.size();j++)
-            {
-                if(strs[j][i]!=c)
-                    break;
-            }
             
+            int j=1;
+            while(j<strs.size() && i<strs[j].size() && c==strs[j][i])
+                j++;
+        
             if(j!=strs.size())
                 break;
+            
             lcp+=c;
         }
 
         return(lcp);
-      
     }
 };
+
+// T.C=O(N*M)   ;   S.C=O(1)
