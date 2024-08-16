ce9a17d0593593a483093240da11bc744ee28a44
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-08-16 15:51:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    void zFunction(vector<int> &z,string &s)
+    {
+        z[0]=0;
+        int l=0,r=0;
+
+        for(int i=1;i<z.size();i++)
+        {
+            if(i<r)
+                z[i]=min(z[i-l],r-i);
+            
+            while(i+z[i]<s.size() && s[i+z[i]]==s[z[i]])
+                z[i]++;
+            
+            if(i+z[i]>r)
+            {
+                l=i;
+                r=i+z[i];
+            }
+        }
+    }
+    
+    int strStr(string haystack, string needle) {
+        // Computing z function of 'needle'
+        vector<int> z(needle.size(),0);
+        zFunction(z,needle);
+
+        // Finding first occurence of 'needle' in 'haystack'
+        int l=0,r=0;
+        for(int i=0;i<=haystack.size()-needle.size();i++)
+        {
+            int len=0;
+            if(i<r)
+                len=min(z[i-l],r-i);
+            
+            while(len<needle.size() && haystack[i+len]==needle[len])
+                len++;
+            
+            if(len==needle.size())
+                return(i);  // Return first occurence
+
+            if(i+len>r)
+            {
+                l=i;
+                r=i+len;
+            }    
+        } 
+        return(-1);     // 'needle' not a part of 'haystack'
+    }
+};
+
+// T.C=O(N)    ;   S.C=O(N)
+// Z FUNCTION
+// PATTERN - STRING MATCHING
