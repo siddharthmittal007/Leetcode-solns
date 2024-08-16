60bc826faeab547c61cd1ef6812490907636084d
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-08-16 16:44:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (29.33%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        // Combining strings 'needle' and 'haystack' with a unique seperator '#'
+        string s(needle+'#'+haystack);
+        
+        // KMP 
+        vector<int> lsp(s.size(),0);    // Longest Suffix Prefix(LSP) array
+        lsp[0]=0;
+        
+        int q=0;
+        for(int i=1;i<lsp.size();i++)
+        {
+            while(q>0 && s[i]!=s[q])
+                q=lsp[q-1];
+
+            if(s[i]==s[q])
+                q++;
+            
+            lsp[i]=q;
+
+            if(q==needle.size())
+                return(i-(int)needle.size()*2);     // Return first occurence 
+        }
+    
+        return(-1);     // 'needle' not a part of 'haystack'
+    }
+};
+
+// T.C=O(N)    ;   S.C=O(N)
+// Z FUNCTION
+// PATTERN - STRING MATCHING
