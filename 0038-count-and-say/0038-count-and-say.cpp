34275d6526b4c6922c7aee0ec3a6894f43f43702
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-08-19 17:43:13 +0530

    Time: 4 ms (67.91%), Space: 7.9 MB (97.56%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        // Base Case
-        if(n==1)
-            return("1");
+        string *p1=new string("1"),*p2=new string();
 
-        // Recursive Call
-        string s=countAndSay(n-1);
-        
-        // Finding RLE for input 'n' from RLE of 'n-1'
-        string ans;
-
-        int digit=s[0]-'0',pos=0;
-        for(int i=0;i<=s.size();i++)
+        // Finding solutions to successive values of 'i' upto 'n' iteratively
+        for(int i=2;i<=n;i++)
         {
-            if(i==s.size() || s[i]-'0'!=digit)
+            int digit=(*p1)[0]-'0',pos=0;
+            for(int j=0;j<=(*p1).size();j++)
             {
-                ans.push_back('0'+(i-pos)); // Count
-                ans.push_back('0'+digit);   // Character
-                if(i<s.size())    
+                if(j==(*p1).size() || (*p1)[j]-'0'!=digit)
                 {
-                    pos=i;
-                    digit=s[i]-'0';
-                }
-            }    
+                    (*p2).push_back('0'+(j-pos)); // Count
+                    (*p2).push_back('0'+digit);   // Character
+                    if(j<(*p1).size())    
+                    {
+                        pos=j;
+                        digit=(*p1)[j]-'0';
+                    }
+                }    
+            }
+            swap(p1,p2);
+            (*p2).clear();
         }
-        
-        return(ans);
+
+        return(*p1);
     }
 };
 
-// T.C=O(N^2) ;   S.C=O(N)(Recursion stack + temporary string)
+// T.C=O(N^2) ;   S.C=O(N)(temporary extra string)
 
 // IMP - HERE DIGITS TAKE ONLY THE VALUE 1,2,3
 // THUS ANSWER STRINGS CAN BE COMPRESSED TO USE 2 BITS FOR EACH DIGIT IF
 // SPACE OPTIMIZATION IN STORAGE OF STRING REQUIRED 
+
+// IMP - USING POINTERS TO OPTIMIZE TIME OF SWAPPING FOR SUCH CASES
