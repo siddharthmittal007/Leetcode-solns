2d096fb48be6037c44769c50ee2a04a9b5eb2c26
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-09-02 18:10:24 +0530

    Time: 544 ms (88.87%), Space: 182.8 MB (74.43%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,49 +1,49 @@
 class Node{
 public:    
-    Node* digit[2];
+    Node* links[2];
 
     Node(){
-        digit[0]=digit[1]=NULL;
+        links[0]=links[1]=NULL;
     }
 };
 
-class BinaryTree{
+class Trie{
 public:    
     Node *root;
 
-    BinaryTree(){
+    Trie(){
         root=new Node();
     }
 
+    // To insert binary representation of given number to trie
     void insert(int value)
     {
         Node *temp=root;
         for(int i=31;i>=0;i--)
         {
-            int b=(value>>i)&1;
-            if(temp->digit[b]==NULL)
-                temp->digit[b]=new Node();
-            temp=temp->digit[b];
+            int bit=(value>>i)&1;
+            if(temp->links[bit]==NULL)
+                temp->links[bit]=new Node();
+            temp=temp->links[bit];
         }
     }
 
-    int max_XOR(int value)
+    // To find maximum XOR of given number with numbers stored in trie
+    int maxXOR(int value)
     {
-        int ans=0;
         Node *temp=root;
+        
+        int ans=0;
         for(int i=31;i>=0;i--)
         {
-            ans=ans<<1;
-            int b=(value>>i)&1;
-            if(temp->digit[1-b]!=NULL)
+            int bit=(value>>i) & 1;
+            if(temp->links[1-bit]!=NULL)
             {
-                ans+=1;
-                temp=temp->digit[1-b];
+                ans|=1<<i;
+                temp=temp->links[1-bit];
             }
-            else if(temp->digit[b]!=NULL)
-                temp=temp->digit[b];
             else
-                ans+=b;
+                temp=temp->links[bit];
         }
 
         return(ans);
@@ -53,15 +53,20 @@ public:
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        BinaryTree bt;
+        Trie t;
         
+        // Storing binary representations of the number in the trie
         for(int i=0;i<nums.size();i++)
-            bt.insert(nums[i]);
+            t.insert(nums[i]);
 
+        // Finding maximum XOR of two numbers
         int max_val=INT_MIN;
         for(int i=0;i<nums.size();i++)
-            max_val=max(max_val,bt.max_XOR(nums[i]));
+            max_val=max(max_val,t.maxXOR(nums[i]));
         
-        return(max_val);
+        return(max_val);    // Return Maximum XOR
     }
 };
+
+// T.C=O(32*N)  ;   S.C=O(32*N)
+// PATTERN - USING BINARY TRIE TO STORE SET OF BINARY NUMBERS 
