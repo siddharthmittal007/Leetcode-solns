1e1393082e627f512b0e47157a995ff0fe7dc9da
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-10-10 15:53:12 +0530

    Time: 0 ms (100%), Space: 9.8 MB (99.05%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,22 +11,47 @@
  */
 class Solution {
 public:
-    void preorderHelper(TreeNode *node,vector<int> &ans)
+    // Utility function
+    TreeNode* inorderPredecessor(TreeNode *curr)
     {
-        if(node!=nullptr)
-        {
-            ans.push_back(node->val);
-            preorderHelper(node->left,ans);
-            preorderHelper(node->right,ans);
-        }
+        TreeNode *temp=curr->left;
+        while(temp->right!=nullptr && temp->right!=curr)
+            temp=temp->right;
+        return(temp);
     }
     
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>  ans;
-        preorderHelper(root,ans);
+        vector<int>  ans;       // To store preorder traversal
+
+        // Morris Preorder Traversal
+        TreeNode *curr=root;
+        while(curr!=nullptr)
+        {
+            if(curr->left!=nullptr)
+            {
+                TreeNode *temp=inorderPredecessor(curr);
+                if(temp->right==nullptr)
+                {
+                    ans.push_back(curr->val);
+                    temp->right=curr;       // Create thread
+                    curr=curr->left;
+                }
+                else    // (temp->right==curr)
+                {
+                    temp->right=nullptr;    // Destroy thread
+                    curr=curr->right;
+                }
+            }
+            else
+            {
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
+        
         return(ans);
     }
 };
 
-// T.C=O(N) ;   S.C=O(N)
+// T.C=O(3*N)   ;   S.C=O(1)
 // PATTERN - TREE TRAVERSAL
