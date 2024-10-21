74b7494a39c64f2d8985f5c3ff68c4ea0e0406db
Siddharth Mittal <66960603+siddharthmittal007@users.noreply.github.com>
2024-10-21 16:20:55 +0530

    Time: 3 ms (92.75%), Space: 18.7 MB (5.67%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
@@ -17,30 +18,31 @@ public:
             return(0);
 
         // level order traversal
-        int max_width=0;
-        queue<pair<TreeNode*,int>> q;       // Queue of (Node,column)
-        q.push({root,0});
+        long long s=0,e=0;
+        int max_width=1;
+        queue<pair<TreeNode*,long long>> q;       // Queue of (Node,column)
+        q.push({root,0});                   // Push root
         q.push({nullptr,NULL});             // Sentinel to mark EOL
         while(!q.empty())
         {
             TreeNode *curr=q.front().first;
-            int col=q.front().second;
+            long long col=q.front().second;
             q.pop();
 
             if(curr==nullptr)
             {
                 if(!q.empty())
                 {
-                    int s=q.front().second, e=q.back().second;
-                    int width=e-s+1;
+                    s=q.front().second,e=q.back().second;
+                    int width=int(e-s+1);
                     if(width>max_width)max_width=width;
                     q.push({nullptr,NULL});     // Sentinel to mark EOL
                 }
             }
             else
             {
-                if(curr->left!=nullptr)q.push({curr->left,2*col});
-                if(curr->right!=nullptr)q.push({curr->right,2*col+1});
+                if(curr->left!=nullptr)q.push({curr->left,2*(col-s)});
+                if(curr->right!=nullptr)q.push({curr->right,2*(col-s)+1});
             }
         }
 
